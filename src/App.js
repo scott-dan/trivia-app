d508013933b90284f67c9ae7931315343b47727d
DanielDScott <danieldscott90@gmail.com>
2020-11-26 01:30:23 -0800

    Added static data for quiz.js to read

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
@@ -12,62 +12,69 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 
 const baseURL = "https://opentdb.com/api.php?";
+
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        slider: 10,
-        category: "",
-        difficulty: "easy",
-        questionType: "any"
-      };
-    }
-
-    updateSlider = (event, newSlider) => {
-      this.setState({ slider: newSlider });
+  constructor(props) {
+    super(props);
+    this.state = {
+      slider: 10,
+      category: "",
+      difficulty: "easy",
+      questionType: "any",
     };
+  }
 
-    updateCategory = (event) =>  {
-      this.setState({ category: event.target.value });
-    };
+  updateSlider = (event, newSlider) => {
+    this.setState({ slider: newSlider });
+  };
 
-    updateDifficulty = (event) => {
-      this.setState({ difficulty: event.target.value });
-    };
+  updateCategory = (event) => {
+    this.setState({ category: event.target.value });
+  };
 
-    updateQuestionType = (event) =>  {
-      this.setState({ questionType: event.target.value });
-    };
-  
-    render() {
-      var quizURL = createURL(baseURL, this.state.slider, this.state.category, this.state.difficulty, this.state.questionType);
-      return (
-        <div className="App">
-         <header className="App-header">
-           <img
-              src="http://www.pngmart.com/files/8/Colorful-Smoke-Transparent-Background.png"
-              className="App-logo"
-              alt="logo"
-           />
-           <Container maxWidth="sm">
-              <Box my={3}>
-               <Typography variant="h4" component="h1" gutterBottom>
-                  Welcome to The Trivia App!
-                </Typography>
-                <p>{quizURL}</p>
-              </Box>
-           </Container>
-           <div className='input'>
-             <Grid container spacing={1}>
-                <Grid item xs={12}>
-                  <Paper className='paper'>
-                    <Typography id="qCountSelect" gutterBottom>
-                     Select the Number of Questions to Play
-                   </Typography>
-                    <Slider
+  updateDifficulty = (event) => {
+    this.setState({ difficulty: event.target.value });
+  };
+
+  updateQuestionType = (event) => {
+    this.setState({ questionType: event.target.value });
+  };
+
+  render() {
+    var quizURL = createURL(
+      baseURL,
+      this.state.slider,
+      this.state.category,
+      this.state.difficulty,
+      this.state.questionType
+    );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="http://www.pngmart.com/files/8/Colorful-Smoke-Transparent-Background.png"
+            className="App-logo"
+            alt="logo"
+          />
+          <Container maxWidth="sm">
+            <Box my={3}>
+              <Typography variant="h4" component="h1" gutterBottom>
+                Welcome to The Trivia App!
+              </Typography>
+              <p>{quizURL}</p>
+            </Box>
+          </Container>
+          <div className="input">
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <Paper className="paper">
+                  <Typography id="qCountSelect" gutterBottom>
+                    Select the Number of Questions to Play
+                  </Typography>
+                  <Slider
                     key={`slider-${this.state.slider}`}
                     defaultValue={this.state.slider}
-            //        getAriaValueText={valuetext}
+                    //        getAriaValueText={valuetext}
                     aria-labelledby="qCountSelect"
                     step={1}
                     marks
@@ -80,17 +87,22 @@ class App extends Component {
                 </Paper>
               </Grid>
               <Grid item xs={4}>
-                <Paper className='paper'>
+                <Paper className="paper">
                   <InputLabel htmlFor="grouped-native-select">
                     Category
                   </InputLabel>
-                  <Select native defaultValue={this.state.category} id="grouped-native-select" onChange={this.updateCategory}>
+                  <Select
+                    native
+                    defaultValue={this.state.category}
+                    id="grouped-native-select"
+                    onChange={this.updateCategory}
+                  >
                     <option value="">Any</option>
                     <optgroup label="General">
                       <option value="9">General Knowledge</option>
                       <option value="25">Art</option>
                     </optgroup>
-  
+
                     <optgroup label="Entertainment">
                       <option value="10">Books</option>
                       <option value="11">Film</option>
@@ -100,7 +112,7 @@ class App extends Component {
                       <option value="15">Video Games</option>
                       <option value="16">Board Games</option>
                     </optgroup>
-  
+
                     <optgroup label="Science">
                       <option value="17">Nature</option>
                       <option value="18">Computers</option>
@@ -110,7 +122,7 @@ class App extends Component {
                 </Paper>
               </Grid>
               <Grid item xs={4}>
-                <Paper className='paper'>
+                <Paper className="paper">
                   <FormLabel component="legend">Quiz Difficulty:</FormLabel>
                   {/*
                   <RadioGroup
@@ -136,7 +148,12 @@ class App extends Component {
                     />
                   </RadioGroup>
                   */}
-                  <Select native defaultValue="easy" id="grouped-native-select" onChange={this.updateDifficulty}>
+                  <Select
+                    native
+                    defaultValue="easy"
+                    id="grouped-native-select"
+                    onChange={this.updateDifficulty}
+                  >
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>
@@ -144,8 +161,10 @@ class App extends Component {
                 </Paper>
               </Grid>
               <Grid item xs={4}>
-                <Paper className='paper'>
-                  <FormLabel component="legend">Select Question Type:</FormLabel>
+                <Paper className="paper">
+                  <FormLabel component="legend">
+                    Select Question Type:
+                  </FormLabel>
                   {/*}
                   <FormGroup>
                     <FormControlLabel
@@ -170,7 +189,12 @@ class App extends Component {
                     />
                   </FormGroup>
                     */}
-                  <Select native defaultValue="any" id="grouped-native-select" onChange={this.updateQuestionType}>
+                  <Select
+                    native
+                    defaultValue="any"
+                    id="grouped-native-select"
+                    onChange={this.updateQuestionType}
+                  >
                     <option value="any">Any</option>
                     <option value="boolean">True/False</option>
                     <option value="multiple">Mutliple Choice</option>
@@ -178,24 +202,53 @@ class App extends Component {
                 </Paper>
               </Grid>
               <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => (fetchQuestions(quizURL))}
-                    //onClick={() => (window.location.href = "questions.html")}
-                  >
-                    Start Quiz!
-                  </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => fetchQuestions(quizURL)}
+                  //onClick={() => (window.location.href = "questions.html")}
+                >
+                  Start Quiz!
+                </Button>
               </Grid>
             </Grid>
           </div>
         </header>
       </div>
-    )
+    );
   }
 }
 
 export default App;
+
+export var QuizData = [
+  {
+    category: "Entertainment: Comics",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the full first name of the babysitter in Calvin and Hobbes?",
+    correct_answer: "Rosalyn",
+    incorrect_answers: ["Rose", "Ruby", "Rachel"],
+  },
+  {
+    category: "Animals",
+    type: "boolean",
+    difficulty: "medium",
+    question: "An octopus can fit through any hole larger than its beak.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science: Mathematics",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "Which of the following famous mathematicians died in a duel at the age of 20?",
+    correct_answer: "Galois",
+    incorrect_answers: ["Abel", "Euler", "Gauss"],
+  },
+];
 
 function createURL(base, count, category, difficulty, type) {
   var apiURL = base;
@@ -217,19 +270,13 @@ function fetchQuestions(url) {
   fetch(url)
     .then((response) => response.json())
     .then((questions) => {
-      console.log("Questions", questions);
-      //listData(questions);
+      QuizData = questions;
+      console.log(QuizData);
     })
     .catch((error) => {
       console.log("Request Failed", error);
       //NEED TO ADD SOME SORT OF OUTPUT HERE FOR DIFFERENT RESPONSE CODES
     });
-}
-
-function listData(data) {
-  for (let i in data.results) {
-    //output.innerHTML += createListItem(data.results[i]);
-  }
 }
 
 function createListItem(data) {
