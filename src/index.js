14987de99199ab8a6cccc078942e7bbbea357c0a
PhilChen12 <60991840+PhilChen12@users.noreply.github.com>
2020-11-27 21:32:31 -0800

    remove app2 import lol

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App2 from './App2';
 
 ReactDOM.render(
   //App renders options page
