3024b17c499f30d8151356c87bb49388f687a071
rickpdx <rcruz@pdx.edu>
2020-11-28 03:28:38 -0800

    Commented out App2 in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App2 from './App2';
+//import App2 from './App2';
 
 ReactDOM.render(
   //App renders options page
